7e306a9b2de07a3c92f6eefdb230b1022249bfca
lancarvalho <53114760+lancarvalho@users.noreply.github.com>
2026-03-31 22:12:13 -0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo: React.FC = () => (
   <div className="flex items-center">
-    <img src="https://www.renatorgomes.com/backup/valeapoio/logo-header-and-footer.png" alt="Vale Apoio Logo" className="h-8 w-auto" />
+    <img src="https://shemaic.com.br/valeapoio/logo-header-and-footer.png" alt="Vale Apoio Logo" className="h-8 w-auto" />
   </div>
 );
 
